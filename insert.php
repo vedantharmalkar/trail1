bcb5b936ebfdc7019d915fd8d3865b243778a151
Vedant Harmalkar <85630091+vedantharmalkar@users.noreply.github.com>
2022-02-23 14:50:15 +0530

    Update insert.php

--- insert.php
+++ insert.php
@@ -4,16 +4,29 @@
 include "connect.php";
 if(isset($_POST['SubmitButton'])){ 
   $rollno =  $_REQUEST['rollno'];
-  $name =  $_REQUEST['sname'];
-  $class = $_REQUEST['sclass'];
- 
-  $sql = "INSERT INTO details
-  values( '$rollno', '$name','$class' )";
-  if (mysqli_query($conn, $sql)) {
+  $Name =  $_REQUEST['sname'];
+  $address = $_REQUEST['address'];
+  $phone = $_REQUEST['phone'];
+  
+  $sub1s1= $_REQUEST['sub1s1'];
+  $sub2s1= $_REQUEST['sub2s1'];
+  $sub1s2 = $_REQUEST['sub1s2'];
+  $sub2s2= $_REQUEST['sub2s2'];
+  $sql1 = "INSERT INTO student
+  values(  '$Name', '$address', '$phone' )";
+  if (mysqli_query($conn, $sql1)) {
     echo "<br>New record inserted successfully";
   } else {
-    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    echo "Error: " . $sql1 . "<br>" . mysqli_error($conn);
   }
+  $sql2 = "INSERT INTO sem
+  values( '$sub1s1','$sub2s1','$sub1s2','$sub2s2' )";
+  if (mysqli_query($conn, $sql2)) {
+    echo "<br>New record inserted successfully";
+  } else {
+    echo "Error: " . $sql2 . "<br>" . mysqli_error($conn);
+  }
+  
 }  
 ?> 
 </body>
